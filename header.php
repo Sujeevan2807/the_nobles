77998c570e7ee6f9b89754ac9faaddd1ed227dcc
Sujeevan <sujeevbala@gmail.com>
2024-03-26 18:16:18 +0530

    home-page

---
+++ header.php
@@ -0,0 +1,205 @@
+
+<div class="mouseCursor cursor-outer"></div>
+    <div class="mouseCursor cursor-inner"><span>Drag</span></div>
+
+    <!-- Preloader start -->
+    <div id="preloader">
+        <div class="line-loader">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+    </div>
+    <!-- preloader end -->
+
+    <!-- Offcanvas area start -->
+    <div class="fix">
+        <div class="offcanvas__info">
+            <div class="offcanvas__wrapper">
+                <div class="offcanvas__content">
+                    <div class="offcanvas__top mb-40 d-flex justify-content-between align-items-center">
+                        <div class="offcanvas__logo">
+                            <a href="index.html">
+                                <img src="assets/img/logo/logo.png" alt="logo">
+                            </a>
+                        </div>
+                        <div class="offcanvas__close">
+                            <button>
+                                <i class="fal fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="offcanvas__user mb-30 d-xxl-none">
+                        
+                    </div>
+                    <div class="offcanvas__search mb-30">
+                        <form action="#">
+                            <input type="text" placeholder="Search Here">
+                            <button type="submit"><i class="far fa-search"></i></button>
+                        </form>
+                    </div>
+                    <div class="hr-1 mt-30 mb-30 d-xl-none"></div>
+                    <div class="mobile-menu fix mb-3 d-xl-none"></div>
+                    
+                    <div class="offcanvas__btn mb-30 ">
+                        <a class="" href="services.html" style="color: #fff;">  Videos</a>
+                    </div>
+                    <div class="hr-1 mt-30 mb-30 d-xl-none"></div>
+                    <div class="offcanvas__social">
+                        <div class="ms-footer-social mb-0">
+                            <a href="https://www.linkedin.com/" title="Instagram" target="_blank">IN</a>
+                            <a href="https://twitter.com/" title="Twitter" target="_blank">TW</a>
+                            <a href="https://www.facebook.com/" title="Facebook" target="_blank">FB</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="offcanvas__overlay"></div>
+    <div class="offcanvas__overlay-white"></div>
+    <!-- Offcanvas area start -->
+
+    <!-- Header area start -->
+    <header id="header">
+        <div id="header-sticky" class="header__area header-1 ms-header-fixed ms-bg-body">
+            <div class="container-fluid ms-maw-1710">
+                <div class="row align-items-center">
+                    <div class="col-12">
+                        <div class="mega__menu-wrapper p-relative">
+                            <div class="header__main ms-border2 d-flex align-items-center justify-content-between">
+                                <div class="header__logo pt-25 pb-25">
+                                    <a href="index.html">
+                                        <img src="assets/img/logo/logo.png" width="100px" alt="logo not found">
+                                    </a>
+                                </div>
+
+                                <div class="header__right">
+                                    <div class="mean__menu-wrapper">
+                                        <div class="main-menu main-menu-ff-space">
+                                            <nav id="mobile-menu">
+                                                <ul>
+                                                   <!--  <li class="has-dropdown">
+                                                        <a href="index.html">Home</a>
+                                                        <ul class="submenu">
+                                                            <li><a href="index.html">Home Style 01</a></li>
+                                                            <li><a href="index-2.html">Home Style 02</a></li>
+                                                            <li><a href="index-3.html">Home Style 03</a></li>
+                                                            <li class="has-dropdown">
+                                                                <a href="javascript:void(0)">RTL Demos</a>
+                                                                <ul class="submenu">
+                                                                    <li><a href="index-rtl.html">Home Style 01 RTL</a>
+                                                                    </li>
+                                                                    <li><a href="index-2-rtl.html">Home Style 02 RTL</a>
+                                                                    </li>
+                                                                    <li><a href="index-3-rtl.html">Home Style 03 RTL</a>
+                                                                    </li>
+                                                                </ul>
+                                                            </li>
+                                                        </ul> 
+                                                    </li> -->
+                                                    <li>
+                                                        <a href="index.php">Home</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="about.html">About us</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="contact.html">Contact us</a>
+                                                    </li>
+                                                </ul>
+                                            </nav>
+                                            <!-- for wp -->
+                                            <div class="header__hamburger ml-50 d-none">
+                                                <button type="button" class="hamburger-btn offcanvas-open-btn">
+                                                    <span>01</span>
+                                                    <span>01</span>
+                                                    <span>01</span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="header__action-inner d-flex align-items-center">
+                                        <div class="enquiry__list ml-10 mr-10 ms-browse-act-wrap p-relative">
+                                            <div class="ms-enquiry-box p-relative d-none d-xl-inline-flex">
+                                                <a href="#"><i class="flaticon-star icon"></i> <span
+                                                        class="text">Videos</span> <span class="number">03</span></a>
+                                            </div>
+                                            <div class="ms-browse-act-item-wrap p-absolute">
+                                                <div class="ms-song-item">
+                                                    <div class="ms-song-img p-relative">
+                                                        <a href="genres.html"><img src="assets/img/genres/genres-03.jpg"
+                                                                alt="brand-song"></a>
+                                                    </div>
+                                                    <div class="ms-song-content">
+                                                        <h3 class="ms-song-title"><a href="genres.html">The
+                                                                Sonics
+                                                                Corporate
+                                                                Band</a>
+                                                        </h3>
+                                                    </div>
+                                                </div>
+                                                <div class="ms-song-item">
+                                                    <div class="ms-song-img p-relative">
+                                                        <a href="genres.html"><img src="assets/img/genres/genres-04.jpg"
+                                                                alt="brand-song"></a>
+                                                    </div>
+                                                    <div class="ms-song-content">
+                                                        <h3 class="ms-song-title"><a href="genres.html">The
+                                                                Northern
+                                                                Lights</a>
+                                                        </h3>
+                                                    </div>
+                                                </div>
+                                                <div class="ms-song-item">
+                                                    <div class="ms-song-img p-relative">
+                                                        <a href="genres.html"><img src="assets/img/genres/genres-05.jpg"
+                                                                alt="brand-song"></a>
+                                                    </div>
+                                                    <div class="ms-song-content">
+                                                        <h3 class="ms-song-title"><a href="genres.html">The
+                                                                Sweet
+                                                                The
+                                                                Jets</a>
+                                                        </h3>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--  <div class="header__btn">
+                                            <a href="join.html" class="ms-border-btn"><i
+                                                    class="fa-regular fa-plus"></i>List
+                                                Your Service</a>
+                                        </div> -->
+                                       <!--  <div class="user__acount d-none d-xxl-inline-flex">
+                                            <span>
+                                                <a href="login.html"><i class="flaticon-user"></i></a>
+                                            </span>
+                                        </div> -->
+                                    </div>
+                                    <div class="header__hamburger">
+                                        <div class="sidebar__toggle">
+                                            <a class="bar-icon" href="javascript:void(0)">
+                                                <span></span>
+                                                <span></span>
+                                                <span></span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header area end -->
